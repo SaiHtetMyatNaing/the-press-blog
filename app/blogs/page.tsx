a10bb175b8f64b310cc1905c76f22a08e6c800f7
Sai Htet Myat Naing <saihtetmyatnaing4@gmail.com>
2025-11-03 10:22:10 +0700

    Revert "feat: integrate better-auth for authentication and user management"
    This reverts commit f73a3e90054c3304b9c53ee31171019c0481ec80.

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import Categories from "@/components/blog/Categories";
 import Posts from "@/components/blog/Posts";
 
+export const revalidate = 60;
 
 export default async function PostsPage({
   searchParams,
